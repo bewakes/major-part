012d1c573487f5ad131642db1b23343f8c8d2d3f
Chitraketu <aspiring.me25@gmail.com>
2016-08-08 11:56:32 +0545

    aesthetic tree.pdf with class_labels and feature_labels

--- decisionTree.py
+++ decisionTree.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plot
 import numpy
+import pydot
 from sklearn import cross_validation
 from sklearn import tree, preprocessing
+from sklearn.externals.six import StringIO
 my_data = numpy.genfromtxt('ChitraData.csv',delimiter=',')
 
 new_data = [x[1:] for x in my_data[1:]]
@@ -29,7 +31,7 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,target,te
 clf = clf.fit(X_train,y_train)
 print clf.score(X_test,y_test)
 #print clf.predict(X[:1, :])
-from sklearn.externals.six import StringIO
-with open("tree.dot",'w') as f:
-    f = tree.export_graphviz(clf,out_file=f)
-
+feature_names = ['Total_Road_Length(KM)','Irrigation_Ratio(HA)']
+class_names = ['low','medium','high','very_high']
+with open("tree.dot","w") as t:
+    t = tree.export_graphviz(clf,out_file=t,feature_names=feature_names,class_names=class_names,filled=True,rounded=True,special_characters=True)
