db3fef930c0a94ced79c724580f1055e9dbaf2e3
Chitraketu <aspiring.me25@gmail.com>
2016-08-08 11:25:40 +0545

    decision tree modified with fixed depth and validation accuracy

--- decisionTree.py
+++ decisionTree.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plot
 import numpy
+from sklearn import cross_validation
 from sklearn import tree, preprocessing
 my_data = numpy.genfromtxt('ChitraData.csv',delimiter=',')
 
@@ -18,15 +19,16 @@ target = []
 for val in Y:
     for i, c in enumerate(classes):
         if val>= c[0] and val < c[1]:
-            print(val, c[0], c[1], True)
+            #print(val, c[0], c[1], True)
             target.append(i)
-        else:
-            print(val, c[0], c[1], False)
+            #print(val, c[0], c[1], False)
 
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X,target)
-print clf.predict(X[:1, :])
+clf = tree.DecisionTreeClassifier(max_depth=7)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,target,test_size=0.20, random_state = 0)
+clf = clf.fit(X_train,y_train)
+print clf.score(X_test,y_test)
+#print clf.predict(X[:1, :])
 from sklearn.externals.six import StringIO
 with open("tree.dot",'w') as f:
     f = tree.export_graphviz(clf,out_file=f)
