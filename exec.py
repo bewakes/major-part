7341abce335db8f1bc9efdb85591bacf5fd9f446
Chitraketu <aspiring.me25@gmail.com>
2016-08-13 21:14:57 +0545

    here is the code with the generic implementation of the k-prototype model

--- exec.py
+++ exec.py
@@ -1,3 +1,5 @@
 from kmeans import kmeans
+from kprototype import kprototype
 usecols = (1,2)
 kmeans('ChitraData.csv',5,usecols)
+kprototype(source='ChitraData.csv',n_clusters=5,usecols=usecols,categorical=[1])
---
+++ kprototype.py
@@ -0,0 +1,27 @@
+import numpy as np
+from kmodes import kprototypes
+
+'''
+function: this is an implementation of k-prototypes model
+input: source-> csv file,
+       categorical->array of index of columns which might be categorical type of data attributes
+       n_clusters->number of clusters needed to be obtained as an output of model
+       usecols-> tuple consisting of index of the columns in csv which needs to be used which is 'None' by default
+output: returns the array of centroids and the seperate array for the categorical modes.
+'''
+def kprototype(source,categorical,n_clusters,usecols=None):
+    syms = np.genfromtxt(source,dtype=str,delimiter=',',usecols=usecols)[0,:]
+    data = np.genfromtxt(source,dtype=object,delimiter=',',usecols=usecols)[1:,:]
+    print syms
+    data = data.astype(float)
+    kproto = kprototypes.KPrototypes(n_clusters=n_clusters,init='Cao',verbose=2)
+    clusters = kproto.fit_predict(data,categorical=categorical)
+
+    print(kproto.cluster_centroids_)
+    print(kproto.enc_map_)
+
+    print(kproto.cost_)
+    print(kproto.n_iter_)
+
+
+    return kproto.cluster_centroids_
